81d4d6ce500c64d0ac6bbb50cc4e8c333c5969b9
tscott-au <tim@nepelle.com.au>
2017-05-02 10:29:18 +1000

    added modules and setup

---
+++ test/common.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// global.coMocha = require('co-mocha')
+global.chai = require('chai')
+// global.chaiAsPromised = require('chai-as-promised')
+// global.sinon = require('sinon')
+// global.sinonChai = require('sinon-chai')
+
+// global.chai.use(global.chaiAsPromised)
+// global.chai.use(global.sinonChai)
+global.expect = global.chai.expect
